b0747ab0ca838081df8e6635436b743f80f30eaa
monochromegane <dev.kuro.obi@gmail.com>
2013-11-14 00:59:37 +0900

    Add method for access resource.

--- lib/saucer/resource.rb
+++ lib/saucer/resource.rb
@@ -2,5 +2,15 @@ module Saucer
   class Resource
     def initialize(resource)
     end
+    def index
+    end
+    def show
+    end
+    def create
+    end
+    def update
+    end
+    def destroy
+    end
   end
 end
--- spec/saucer_spec.rb
+++ spec/saucer_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe Saucer do
   subject { Saucer.resouce_of(:blogs) }
   it { should be_a_kind_of(Saucer::Resource) }
+  it { should respond_to(:index) }
+  it { should respond_to(:show) }
+  it { should respond_to(:create) }
+  it { should respond_to(:update) }
+  it { should respond_to(:destroy) }
 end
